335ec3a9e88dbab0832bba4528b8940d2524d525
Schlump <you@example.com>
2017-12-16 21:51:53 +0100

    added lable and title

--- Python/Sentinel/sentinelsat_visualizations.py
+++ Python/Sentinel/sentinelsat_visualizations.py
@@ -7,16 +7,25 @@ import io
 import matplotlib.pyplot as plt
 from sentinelsat.sentinel import SentinelAPI, read_geojson, geojson_to_wkt
 from datetime import date
-
+import re
 
 
 
 
 def show_quicklook(id_,username,password):
-    url = "https://scihub.copernicus.eu/apihub/odata/v1/Products('{}')/Products('Quicklook')/$value".format(id_)
-    bytes_img = requests.session().get(url, auth=(username,password)).content
-    return Image.open(io.BytesIO(bytes_img))
-
+    data = "https://scihub.copernicus.eu/apihub/odata/v1/Products('{}')/Attributes('Sensing%20start')".format(id_)
+    data = requests.session().get(data, auth=(username,password)).text
+    sensingtime = re.search(r'<d:Value[^>]*>([^<]+)</d:Value>',data).group(0)
+    sensingtime = sensingtime.replace('<d:Value>','').replace('</d:Value>','')
+    quicklook = "https://scihub.copernicus.eu/apihub/odata/v1/Products('{}')/Products('Quicklook')/$value".format(id_)
+    bytes_img = requests.session().get(quicklook, auth=(username,password)).content
+    img = Image.open(io.BytesIO(bytes_img))
+    fig, ax = plt.subplots(figsize=(10,10))
+    ax.set_xticks([])
+    ax.set_yticks([])
+    plt.title(id_, fontsize=16)
+    plt.xlabel(sensingtime,fontsize=16)
+    plt.imshow(img)
 
 
 def plot_footprint_folium(GeoDataFrame):
@@ -29,6 +38,4 @@ def plot_footprint_folium(GeoDataFrame):
         folium.Marker(location=([row['geometry'].centroid.y, row['geometry'].centroid.x]
                  ),popup=string).add_to(map_)
                       
-
-
     return map_
